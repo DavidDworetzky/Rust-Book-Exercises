7667aaa16fe3e2d9272e097d79ad0aa1f853a371
DavidDworetzky <djdworetz@gmail.com>
2021-10-31 00:00:51 -0500

    Add hello-cargo exercises

---
+++ hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311.d
@@ -0,0 +1,5 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311: src/main.rs
+
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/hello_cargo/target/debug/hello_cargo: /Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/hello_cargo/src/main.rs
