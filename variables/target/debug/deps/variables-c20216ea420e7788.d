2628061d4d371a4d751c2e2da823a60111a4e8ce
DavidDworetzky <djdworetz@gmail.com>
2021-11-02 23:46:58 -0500

    Add variables exercise from rust-book

---
+++ variables/target/debug/deps/variables-c20216ea420e7788.d
@@ -0,0 +1,5 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/variables/target/debug/deps/variables-c20216ea420e7788: src/main.rs
+
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/variables/target/debug/deps/variables-c20216ea420e7788.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/variables/target/debug/variables: /Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/variables/src/main.rs
