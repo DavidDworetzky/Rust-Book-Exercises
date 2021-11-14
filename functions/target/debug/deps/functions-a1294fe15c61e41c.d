64e340e152571ab96140fb133fa30fb5d1bfac50
DavidDworetzky <djdworetz@gmail.com>
2021-11-14 00:00:24 -0600

    Add functions section from Rust-Book

---
+++ functions/target/debug/deps/functions-a1294fe15c61e41c.d
@@ -0,0 +1,5 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/functions/target/debug/deps/functions-a1294fe15c61e41c: src/main.rs
+
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/functions/target/debug/deps/functions-a1294fe15c61e41c.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/functions/target/debug/functions: /Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/functions/src/main.rs
