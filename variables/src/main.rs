85b50bc986a6b41423c202e00ec5005892392fe7
DavidDworetzky <djdworetz@gmail.com>
2021-11-13 23:51:33 -0600

    Added commits for variable type declarations in rust

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,4 +14,92 @@ fn main() {
     let x = 2.0; // f64
 
     let y: f32 = 3.0; // f32
+
+    /* Data types. Integer division */
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    
+    // multiplication
+    let product = 4 * 30;
+    
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+    
+    // remainder
+    let remainder = 43 % 5;
+
+    /* Data Types Section. 3.2. Booleans.*/
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+
+    /* char types*/
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    /*tuple types.. Compound types*/
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    /* arrays */
+
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    /*explicit type declaration*/
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    /*repeating syntax */
+
+    let a = [3; 5];
+
+    /*out of bound array access */
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+
 }
