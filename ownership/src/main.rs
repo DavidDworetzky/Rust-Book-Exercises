96c7ce02fbcd7f82180ca5b48b52f1010ffae740
DavidDworetzky <djdworetz@gmail.com>
2021-11-22 23:38:57 -0600

    Ownership changes wit hmutable collection

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    {   // s is not valid here, it's not yet declared
+        // s is valid from this point forward
+        let s = "hello";
+    }   //this scope is now oever, and si s no longer valid
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); //push_str() appends a literal to a string
+
+    println!("{}", s); //this will print hello world
 }
