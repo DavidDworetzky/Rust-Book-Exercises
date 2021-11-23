95cbe8f7fb3d5349d2a4c9e2ca3cf587003f411b
DavidDworetzky <djdworetz@gmail.com>
2021-11-22 23:51:03 -0600

    Ownership and functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
-    {   // s is not valid here, it's not yet declared
-        // s is valid from this point forward
-        let s = "hello";
-    }   //this scope is now oever, and si s no longer valid
+    let s = String::from("hello");  // s comes into scope
 
-    let mut s = String::from("hello");
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
 
-    s.push_str(", world!"); //push_str() appends a literal to a string
+    let x = 5;                      // x comes into scope
 
-    println!("{}", s); //this will print hello world
+    makes_copy(x);                  // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
 
-    let x = 5;
-    let y = x;
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
 
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
 
-    let s1 = String::from("hello");
-    let s2 = s1;
-
-    //printing invalidated reference
-
-    println!("{}, world!", s1);
-}
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
