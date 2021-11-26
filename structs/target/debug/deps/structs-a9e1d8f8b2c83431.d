1f68cbec6b3b75362d7b65deb55176337fc55a54
DavidDworetzky <djdworetz@gmail.com>
2021-11-26 00:54:24 -0600

    Add structs chapter to rust book

---
+++ structs/target/debug/deps/structs-a9e1d8f8b2c83431.d
@@ -0,0 +1,5 @@
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/structs/target/debug/deps/structs-a9e1d8f8b2c83431: src/main.rs
+
+/Users/daviddworetzky/Documents/Repos/Rust-Book-Exercises/structs/target/debug/deps/structs-a9e1d8f8b2c83431.d: src/main.rs
+
+src/main.rs:
