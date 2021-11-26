50bc7f4325506d44de17bc1aab59e261e3bff2a4
DavidDworetzky <djdworetz@gmail.com>
2021-11-26 00:57:07 -0600

    structs changes

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,14 +1,14 @@
 struct User {
-    username: &str,
-    email: &str,
+    username: str,
+    email: str,
     sign_in_count: u64,
     active: bool,
 }
 
 fn main() {
     let user1 = User {
-        email: "someone@example.com",
-        username: "someusername123",
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
