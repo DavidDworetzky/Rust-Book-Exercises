6f768482a64b193eac63b16ff2df24dfe6ba5a09
DavidDworetzky <djdworetz@gmail.com>
2021-11-29 00:01:05 -0600

    Rectangle example of structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,29 +1,15 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
-    //in rust, we can use spread-like syntax to initialize one struct from another
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+
+    dbg!(&rect1);
 }
